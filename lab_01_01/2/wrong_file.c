2f80a874082ac980d6bde936dfe36d430cf5b5e7
Ivan Spasenov <spasenovivan@mail.ru>
2017-03-11 00:43:43 +0300

    Changed wrong_file.c

--- lab_01_01/2/wrong_file.c
+++ lab_01_01/2/wrong_file.c
@@ -1,16 +1,7 @@
 #include <stdio.h>
-#include <stdio.h>
-#include <math.h>
-#define PI 3.1415926
-int main(void)
+int main()
 {
-float a, b;  // большая и меньшая сторона пирамиды
-float u;  // угол при большем основании
-float h, s;  // высота и площадь пирамиды
-printf("Введите большую, меньшую стороны трапеции и угол при большей стороне: ");
-scanf("%f %f %f", &a, &b, &u);
-h = (a-b)*tan((PI/180)*u)/2;
-s = h*a/2;
-printf("Площадь равна: %7.3f\n", s);
-return 0;
+ int s;
+ s = 56;
+ printf("Year has %d weeks",s);
 }
