dfb13cb1c9d7c6c878d7e1adefad3c0e49085243
Ivan Spasenov <spasenovivan@mail.ru>
2017-05-20 00:13:39 +0300

    new in lab5

--- lab_05_01/zad1.c
+++ lab_05_01/zad1.c
@@ -6,13 +6,12 @@
     
 #define OK 0
 #define NO_FILE -1
-#define NOT_ALL_ARGUMENTS -2
-#define NUMBER_ITEMS_EXCEEDED -3
-#define NUMBER_UNCORRECT -4
+#define FILE_EMPTY -2
+#define NOT_ALL_ARGUMENTS -3
+#define NUMBER_ITEMS_EXCEEDED -4
 
 int sum_proizved(const int* pb,const int* pe)
 {
-	pe = pe - 1;
     int sum = 0;
     const int *left_border = pb;
     const int *right_border = pe;
@@ -41,32 +40,21 @@ int sum_proizved(const int* pb,const int* pe)
 
 int read_array(FILE *f, int **pb, int **pe)
 {
-    int k = 0;
     int err = OK;
-    if (fscanf(f,"%d",*pb) == 1)
+    while((*pe-*pb <= NUMBER_ITEMS) && (fscanf(f,"%d",*pe) == 1))
     {
-        *pe = *pb; 
-		//printf("%d\n",**pe);
-		*pe = *pe + 1;
-		
-        k++;
-		while((fscanf(f,"%d",*pe) == 1) && (k <= NUMBER_ITEMS))
-		{
-			//printf("%d\n",**pe);
-			*pe = *pe + 1;
-			
-			k++;
-		}
-
-		if (k > NUMBER_ITEMS)
-		{
-			err = NUMBER_ITEMS_EXCEEDED;
-		}
-	}
-	else
-	{
-		err = NUMBER_UNCORRECT;
-	}
+        *pe = *pe + 1;
+    }
+    
+    if (*pe == *pb)
+    {
+        err = FILE_EMPTY;
+    }
+    
+    if (*pe-*pb > NUMBER_ITEMS)
+    {
+        err = NUMBER_ITEMS_EXCEEDED;
+    }
     return err;        
 }
 
@@ -81,36 +69,38 @@ int main(int argc, char** argv)
     int *pe = a;
     
     if (argc != 2)
+    {
+        fprintf(stderr, "main1.exe <file-name>\n");
+        err = NOT_ALL_ARGUMENTS;
+    }
+    else
+    {
+        f = fopen(argv[1], "r");
+        if (f == NULL)
         {
-            fprintf(stderr, "main1.exe <file-name>\n");
-            err = NOT_ALL_ARGUMENTS;
+            fprintf(stderr, "%s\n",strerror(errno));
+            err = NO_FILE;
         }
         else
         {
-            f = fopen(argv[1], "r");
-            if (f == NULL)
+            err = read_array(f, &pb, &pe);
+            if (err == FILE_EMPTY)
             {
-                fprintf(stderr, "%s\n",strerror(errno));
-                err = NO_FILE;
+                printf("File is empty");    
             }
             else
             {
-                err = read_array(f, &pb, &pe);
-				if (err == NUMBER_UNCORRECT)
-				{
-					printf("First element in array uncorrect or array is empty");	
-				}
-				else
-				{
-					if (err == NUMBER_ITEMS_EXCEEDED)
-					{
-						printf("The number of elements exceeded %d Only %d elements will be taken into account\n",NUMBER_ITEMS,NUMBER_ITEMS);
-					}
-					sum = sum_proizved(pb, pe);
-					printf("The sum of products of positive and negative elements = %d",sum);
-				}
+                if (err == NUMBER_ITEMS_EXCEEDED)
+                {
+                    printf("The number of elements exceeded %d Only %d elements will be taken into account\n",NUMBER_ITEMS,NUMBER_ITEMS);
+                }
+                sum = sum_proizved(pb, pe - 1);
+                printf("The sum of products of positive and negative elements = %d",sum);
             }
             fclose(f);
-        }    
+        }
+        
+    }    
     return err;
 }
+    
