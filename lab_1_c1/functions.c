3e1861f7be8c2c3e2b24cabacabb8eee5c967b45
U-VLAB\user-lab01 <user-lab01@LAB-01-64.vlab.bmstu.ru>
2017-09-06 15:23:36 +0300

    add files

---
+++ lab_1_c1/functions.c
@@ -0,0 +1,180 @@
+#include <math.h>
+#include <assert.h>
+#include <stdio.h>
+
+#define INCORRECT_EPSILON -1
+#define NO_INTERSECTIONS -2
+
+float func1(float x)
+{
+    float y = exp(-x+2);
+    return y;
+}
+
+// Вторая пользовательская функция
+float func2(float x)
+{
+    float y = exp(x);
+    return y;
+}
+    
+// Третья пользовательская функция    
+float func3(float x)
+{
+    float y = 3*x+2;
+    return y;
+}
+
+// Первая пользовательская функция
+float eq1(float x)
+{
+    float y = func1(x)-func2(x);
+    return y;
+}
+
+// Вторая пользовательская функция
+float eq2(float x)
+{
+    float y = func2(x)-func3(x);
+    return y;
+}
+    
+// Третья пользовательская функция    
+float eq3(float x)
+{
+    float y = func3(x)-func1(x);
+    return y;
+}
+
+
+
+float PolovDel(float (*func)(float), float a, float b, float eps)
+{
+    float x1 = 0; 
+    float x2 = (a + b) / 2;  
+
+    while (fabs(x2 - x1) >= eps)
+	{
+        x1 = x2;
+      
+        if (func(a) * func(x2) <= 0)
+            b = x2;
+        else
+		{
+			if (func(x2) * func(b) <= 0)
+				a = x2;
+		}
+        x2 = (a + b) / 2;
+	}
+    return x1;
+}
+
+
+
+// Функция, находящая пересечения пользовательских функций
+int intersection(float *x1, float *x2, float *x3, float (*eq1)(float), float (*eq2)(float),float (*eq3)(float))
+{
+    float begin = -1;   
+    float end = 1;    
+    float dx = 0.01;
+    float nach = begin;
+	float kon = nach+dx;
+    int bool1 = 0;
+    int bool2 = 0;
+    int bool3 = 0;
+    while (kon <= end)
+    {
+   
+        if (eq1(nach)*eq1(kon) <= 0)
+        {
+            *x1 = PolovDel(eq1,nach,kon,0.00001);
+            bool1 = 1;
+        }
+        if (eq2(nach)*eq2(kon) <= 0)
+        {
+            *x2 = PolovDel(eq2,nach,kon,0.00001);
+            bool2 = 1;
+        }    
+        if (eq3(nach)*eq3(kon) <= 0)
+        {
+            *x3 = PolovDel(eq3,nach,kon,0.00001);
+            bool3 = 1;
+        }
+		nach = kon;
+        kon = kon + dx;
+    }
+	printf("###   %f   %f   %f ###",*x1,*x2,*x3);
+    if (bool1 == 0 || bool2 == 0 || bool3 == 0)
+    {
+        return NO_INTERSECTIONS;
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+//Функция находящая интеграл по методу трапеций для данного колиества разбиений
+float trapez_method(float a, float b, int n, float (*func)(float))
+{
+    float h = (b-a)/n;
+    float s = 0;
+    float x = 0;
+    
+    assert(n > 1);
+    assert(func);
+    
+    for (int i = 1; i < n; i++) 
+    {
+        x = a + h*i;
+        s += func(x);
+    }
+    s = fabs(h * ((func(a)+func(b))/2 + s));
+    return s;
+}
+
+//Функция находящая интеграл с заданной точностью
+float integral(float a, float b, float eps, float (*func)(float))
+{
+    assert(0 < eps && eps < 1);
+    assert(func);
+    
+    float s1 = 0;
+    int n = 2;
+    float s2 = trapez_method(a,b,n,func);
+    while (fabs((s2-s1)/s1) > eps)
+    {
+        n *= 2;
+        s1 = s2;
+        s2 = trapez_method(a,b,n,func);
+    }
+    return s1;
+}
+    
+// Функция, находящая площадь криволинейного треугольника
+float square(float x1, float x2, float x3, float eps, float (*eq1)(float), float (*eq2)(float),float (*eq3)(float))
+{
+    float s1 = integral(x1,x3,eps,func1);
+    float s2 = integral(x1,x2,eps,func2);
+    float s3 = integral(x2,x3,eps,func3);
+	printf("###   %f   %f   %f ###",s1,s2,s3);
+    float answer = 0;
+    if ((x2 <= x1 && x1 <= x3) || (x3 <= x1 && x1 <= x2))
+    {
+        answer = fabs(s1 + s2 - s3);
+        
+    }
+    if ((x1 <= x2 && x2 <= x3) || (x3 <= x2 && x2 <= x1))
+    {
+        answer = fabs(s1 + s3 - s2);
+        
+    }
+    if ((x1 <= x3 && x3 <= x2) || (x2 <= x3 && x3 <= x1))
+    {
+        answer = fabs(s1 + s2 - s3);
+    }
+    return answer;
+}
--- lab_1_c1/main.c
+++ lab_1_c1/main.c
@@ -1,132 +1,10 @@
 #include <stdio.h>
 #include <math.h>
 #include <assert.h>
+#include "functions.h"
 
 #define INCORRECT_EPSILON -1
 #define NO_INTERSECTIONS -2
-
-// Первая пользовательская функция
-float func1(float x)
-{
-    float y = exp(-x+2);
-    return y;
-}
-
-// Вторая пользовательская функция
-float func2(float x)
-{
-    float y = exp(x);
-    return y;
-}
-    
-// Третья пользовательская функция    
-float func3(float x)
-{
-    float y = 3*x+2;
-    return y;
-}
-
-// Функция, находящая пересечения пользовательских функций
-int intersection(float *x1, float *x2, float *x3, float (*func1)(float), float (*func2)(float),float (*func3)(float))
-{
-    float begin = -1;   
-    float end = 1;    
-    float dy = 0.001;
-    float dx = 0.000001;
-    float x = begin;
-    float y1,y2,y3;
-    int bool1 = 0;
-    int bool2 = 0;
-    int bool3 = 0;
-    while (x <= end)
-    {
-        y1 = func1(x);
-        y2 = func2(x);
-        y3 = func3(x);
-        if (fabs(y1-y2)<dy)
-        {
-            *x1 = x;
-            bool1 = 1;
-        }
-        if (fabs(y1-y3)<dy)
-        {
-            *x2 = x;
-            bool2 = 1;
-        }    
-        if (fabs(y2-y3)<dy)
-        {
-            *x3 = x;
-            bool3 = 1;
-        }
-        x = x + dx;
-    }
-    if (bool1 == 0 || bool2 == 0 || bool3 == 0)
-    {
-        return NO_INTERSECTIONS;
-    }
-    return 0;
-}
-
-//Функция находящая интеграл по методу трапеций для данного колиества разбиений
-float trapez_method(float a, float b, int n, float (*func)(float))
-{
-    float h = (b-a)/n;
-    float s = 0;
-    float x = 0;
-    
-    assert(n > 1);
-    assert(func);
-    
-    for (int i = 1; i < n; i++) 
-    {
-        x = a + h*i;
-        s += func(x);
-    }
-    s = fabs(h * ((func(a)+func(b))/2 + s));
-    return s;
-}
-
-//Функция находящая интеграл с заданной точностью
-float integral(float a, float b, float eps, float (*func)(float))
-{
-    assert(0 < eps && eps < 1);
-    assert(func);
-    
-    float s1 = 0;
-    int n = 2;
-    float s2 = trapez_method(a,b,n,func);
-    while (fabs((s2-s1)/s1) > eps)
-    {
-        n *= 2;
-        s1 = s2;
-        s2 = trapez_method(a,b,n,func);
-    }
-    return s1;
-}
-    
-// Функция, находящая площадь криволинейного треугольника
-float square(float x1, float x2, float x3, float eps, float (*func1)(float), float (*func2)(float),float (*func3)(float))
-{
-    float s1 = integral(x1,x2,eps,func1);
-    float s2 = integral(x1,x3,eps,func2);
-    float s3 = integral(x2,x3,eps,func3);
-    float answer = 0;
-    if ((x2 <= x1 && x1 <= x3) || (x3 <= x1 && x1 <= x2))
-    {
-        answer = fabs(s1 + s2 - s3);
-        
-    }
-    if ((x1 <= x2 && x2 <= x3) || (x3 <= x2 && x2 <= x1))
-    {
-        answer = fabs(s1 + s3 - s2);
-        
-    }
-    if ((x1 <= x3 && x3 <= x2) || (x2 <= x3 && x3 <= x1))
-    {
-        answer = fabs(s1 + s2 - s3);
-    }
-    return answer;
-}
 
 int main(void)
 {
@@ -142,7 +20,8 @@ int main(void)
     }
     else
     {
-        err = intersection(&x1,&x2,&x3,func1,func2,func3);
+        err = intersection(&x1,&x2,&x3,eq1,eq2,eq3);
+		
         if (err == NO_INTERSECTIONS)
         {
             printf("There is not triangle");
@@ -150,8 +29,10 @@ int main(void)
         else
         {
             printf("Square triangle = ");
-            printf("%f  ",square(x1,x2,x3,eps,func1,func2,func3));
+            printf("%f  ",square(x1,x2,x3,eps,eq1,eq2,eq3));
+			
         }
     }
+	//printf("   %f   ",PolovDel(eq2,-1,0,0.00001));
     return err;
 }
