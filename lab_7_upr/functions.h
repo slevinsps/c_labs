0960dbd890287c1eac8bd25be2df9ab1e51b28b2
Ivan <spasenovivan@mail.ru>
2017-09-07 21:16:09 +0300

    rename folder

---
+++ lab_7_upr/functions.h
@@ -0,0 +1,16 @@
+#ifndef FUNCTIONS
+#define FUNCTIONS
+    
+float func1(float x);
+float func2(float x);
+float func3(float x);
+float eq1(float x);
+float eq2(float x);
+float eq3(float x);
+float polovin_del(float (*func)(float), float a, float b, float eps);
+int intersection(float *x1, float *x2, float *x3, float (*eq1)(float), float (*eq2)(float),float (*eq3)(float),float begin,float end);
+float trapez_method(float a, float b, int n, float (*func)(float));
+float integral(float a, float b, float eps, float (*func)(float));
+float square(float x1, float x2, float x3, float eps, float (*eq1)(float), float (*eq2)(float),float (*eq3)(float));
+
+#endif // FUNCTIONS
