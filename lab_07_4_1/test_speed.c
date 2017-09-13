e9bcb12947e394054b13d1a28f1781b406278669
Ivan <spasenovivan@mail.ru>
2017-09-13 09:25:10 +0300

    add tests speed

--- lab_07_4_1/test_speed.c
+++ lab_07_4_1/test_speed.c
@@ -24,14 +24,42 @@ unsigned long long tick(void)
 int main(int argc, char** argv)
 {
     int n;
+	int a[10];
     int a1[100];
     int a2[1000];
     int a3[10000];
-    //int a4[100000];
+    int a4[100000];
     //int a5[1000000]; 
   
     unsigned long long tb, te;
+    srand(time(NULL));
+	
+	
+	n = 10;
+    for(int i = 0;i<n;i++)
+    {
+        a[i] = rand() % (100)+1;
+    }
+    tb = tick();
+    //qsort(a1,n,sizeof(int),compare_int);
+    binary_insert(a,n,sizeof(int),compare_int);
+    te = tick();
+    printf("binary_insert 10: %I64d\n", (te - tb) / n);
     
+    
+    for(int i = 0;i<n;i++)
+    {
+        a[i] = rand() % (100)+1;
+    }
+    tb = tick();
+    qsort(a,n,sizeof(int),compare_int);
+    //binary_insert(a1,n,sizeof(int),compare_int);
+    te = tick();
+    printf("qsort 10: %I64d\n", (te - tb) / n);
+    
+    
+    /////////////////
+	
     n = 100;
     for(int i = 0;i<n;i++)
     {
@@ -43,7 +71,7 @@ int main(int argc, char** argv)
     te = tick();
     printf("binary_insert 100: %I64d\n", (te - tb) / n);
     
-    srand(time(NULL));
+    
     for(int i = 0;i<n;i++)
     {
         a1[i] = rand() % (100)+1;
@@ -59,7 +87,7 @@ int main(int argc, char** argv)
     
     
     n = 1000;
-    srand(time(NULL));
+    //srand(time(NULL));
     for(int i = 0;i<n;i++)
     {
         a2[i] = rand() % (100)+1;
@@ -108,7 +136,7 @@ int main(int argc, char** argv)
     printf("qsort 10000: %I64d\n", (te - tb) / n);
     
     //////////////////////
-    /*
+    
     n = 100000;
     //srand(time(NULL));
     for(int i = 0;i<n;i++)
@@ -133,7 +161,7 @@ int main(int argc, char** argv)
     te = tick();
     printf("binary_insert 100000: %I64d\n", (te - tb) / n);
     
-    
+    /*
     
     ////////////////////////
     
