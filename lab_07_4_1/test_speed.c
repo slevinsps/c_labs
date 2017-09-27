65f7b089f19021182326e3c0e09c57f6e2117b6d
U-VLAB\user-lab01 <user-lab01@LAB-01-25.vlab.bmstu.ru>
2017-09-27 15:23:52 +0300

    test

--- lab_07_4_1/test_speed.c
+++ lab_07_4_1/test_speed.c
@@ -11,7 +11,7 @@
 #include "binary_sort.h"
 #include "defines.h"
     
-int compare_int(const void* p, const void* q)
+int compare_int(const void *p, const void *q)
 {
     const int *a = p;
     const int *b = q;
@@ -20,27 +20,27 @@ int compare_int(const void* p, const void* q)
 
 unsigned long long tick(void)
 {
- unsigned long long d;
- __asm__ __volatile__ ("rdtsc" : "=A" (d) );
- return d;
+	unsigned long long d;
+	__asm__ __volatile__ ("rdtsc" : "=A" (d));
+	return d;
 }
 
 
-void print_results(int*a,int n)
+void print_results(int *a, int n)
 {
     unsigned long long tb, te;
     unsigned long long t_mid;
     int b[n];
     t_mid = 0;
     int count = 100;
-    for(int i = 0;i<count;i++)
+    for(int i = 0;i < count;i++)
     {
-        for(int j = 0;j<n;j++)
+        for(int j = 0;j < n;j++)
         {
             b[j] = a[j];
         }
         tb = tick();
-        mysort(b,n,sizeof(int),compare_int);
+        mysort(b, n, sizeof(int), compare_int);
         te = tick();
         if (te >= tb)
         {
@@ -49,17 +49,17 @@ void print_results(int*a,int n)
         else
             count--;
     }
-    printf("mysort %d: %I64d\n",n, t_mid / count);
+    printf("mysort %d: %I64d\n", n, t_mid / count);
     
     t_mid = 0;
-    for(int i = 0;i<count;i++)
+    for(int i = 0;i < count;i++)
     {
-        for(int j = 0;j<n;j++)
+        for(int j = 0;j < n;j++)
         {
             b[j] = a[j];
         }
         tb = tick();
-        qsort(b,n,sizeof(int),compare_int);
+        qsort(b, n, sizeof(int), compare_int);
         te = tick();
         if (te >= tb)
         {
@@ -73,7 +73,7 @@ void print_results(int*a,int n)
 }
 
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     int a[10];
     int a1[100];
@@ -83,100 +83,100 @@ int main(int argc, char** argv)
     srand(time(NULL));
     printf("В случайном порядке:\n");
     n = 10;
-    for(int i = 0;i<n;i++)
+    for(int i = 0;i < n;i++)
     {
-        a[i] = rand() % (100)+1;
+        a[i] = rand() % (100) + 1;
     }
-    print_results(a,n);
+    print_results(a, n);
 
     n = 100;
-    for(int i = 0;i<n;i++)
+    for(int i = 0;i < n;i++)
     {
-        a1[i] = rand() % (100)+1;
+        a1[i] = rand() % (100) + 1;
     }
-    print_results(a1,n);
+    print_results(a1, n);
 
     n = 1000;
 
-    for(int i = 0;i<n;i++)
+    for(int i = 0;i < n;i++)
     {
-        a2[i] = rand() % (100)+1;
+        a2[i] = rand() % (100) + 1;
     }
-    print_results(a2,n);
+    print_results(a2, n);
         
     /////////////////
     printf("\n\nВ порядке возрастания:\n");
     n = 10;
-    for(int i = 0;i<n;i++)
+    for(int i = 0;i < n;i++)
     {
         a[i] = i;
     }
-    print_results(a,n);
+    print_results(a, n);
 
     n = 100;
 
-    for(int i = 0;i<n;i++)
+    for(int i = 0;i < n;i++)
     {
         a1[i] = i;
     }
-    print_results(a1,n);
+    print_results(a1, n);
 
     n = 1000;
 
-    for(int i = 0;i<n;i++)
+    for(int i = 0;i < n;i++)
     {
         a2[i] = i;
     }
-    print_results(a2,n);
+    print_results(a2, n);
     
     /////////////////
     printf("\n\nВ порядке убывания:\n");
     n = 10;
 
-    for(int i = 0;i<n;i++)
+    for(int i = 0;i < n;i++)
     {
-        a[i] = n-i;
+        a[i] = n - i;
     }
     print_results(a,n);
     
     n = 100;
 
-    for(int i = 0;i<n;i++)
+    for(int i = 0;i < n;i++)
     {
-        a1[i] = n-i;
+        a1[i] = n - i;
     }
-    print_results(a1,n);
+    print_results(a1, n);
 
     n = 1000;
 
-    for(int i = 0;i<n;i++)
+    for(int i = 0;i < n;i++)
     {
-        a2[i] = n-i;
+        a2[i] = n - i;
     }
-    print_results(a2,n);
+    print_results(a2, n);
     
     /////////////////
     printf("\n\nОдинаковые значения:\n");
     n = 10;
-    for(int i = 0;i<n;i++)
+    for(int i = 0;i < n;i++)
     {
         a[i] = 5;
     }
-    print_results(a,n);
+    print_results(a, n);
 
     n = 100;
 
-    for(int i = 0;i<n;i++)
+    for(int i = 0;i < n;i++)
     {
         a1[i] = 5;
     }
-    print_results(a1,n);
+    print_results(a1, n);
 
     n = 1000;
 
-    for(int i = 0;i<n;i++)
+    for(int i = 0;i < n;i++)
     {
         a2[i] = 5;
     }
-    print_results(a2,n);
+    print_results(a2, n);
 }
