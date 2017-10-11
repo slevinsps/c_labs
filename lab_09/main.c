6998cd0e663ade3720e0092453cde8dff4f06737
U-VLAB\user-lab01 <user-lab01@LAB-01-25.vlab.bmstu.ru>
2017-10-11 15:23:52 +0300

    add gost

--- lab_09/main.c
+++ lab_09/main.c
@@ -1,10 +1,12 @@
 # include <stdio.h>
 # include <string.h>
 # include <stdlib.h>
+# include <ctype.h>
 
 # include "str.h"
 # include "sort.h"
 
+# define SEPP ",.?!" 
 
 void input(char s[]) 
 {
@@ -16,6 +18,37 @@ void input(char s[])
     }
     s[i] = '\0';
 }
+
+
+void gost(char s[],char s2[]) // функция ГОСТ
+{
+	int len = strlen(s);
+	int k = 0;
+	int counter = 0;
+	s[0] = toupper(s[0]);
+	for (int i = 0; i < len-1;i++)
+	{
+		
+		if (s[i] == ' ')
+		{
+			k++;
+			if (k == 1)
+			{
+				
+				s2[counter] = s[i];
+				counter++;
+			}
+	    }
+		else
+		{
+			k = 0;
+			s2[counter] = s[i];
+			counter++;
+		}
+		
+	}
+}
+
 
 
 int main(void)
@@ -32,6 +65,13 @@ int main(void)
     split_words(s, words);
 	sort(words, n);
 	for(int i = 0; i < n;i++)
-		printf("%s ", words[i]);
+		printf("%s ", words[i]); 
+	
+	
+
+	char s2[] = "hello      hjhjhj,  kljlk!";
+	char s3[strlen(s2)];
+	gost(s2,s3);
+	printf("%s",s3);
 
 }
