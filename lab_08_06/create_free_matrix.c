c0efbde41bbd7478d21c0957feee72ea09a443eb
Ivan <spasenovivan@mail.ru>
2017-10-12 20:48:51 +0300

    test

---
+++ lab_08_06/create_free_matrix.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+    
+#include "defines.h"
+void free_matrix_rows(double **data, double n)
+{
+    for (int i = 0;i < n; i++)
+    {
+        free(data[i]);        
+    }
+    free(data);    
+}
+    
+double** allocate_matrix_row(int n,int m)
+{
+    double **data = calloc(n,sizeof(double*));
+    if (!data)
+        return NULL;
+    for (int i = 0;i < n;i++)
+    {
+        data[i] = malloc(m*sizeof(double));
+        if (!data[i])
+        {
+            free_matrix_rows(data,n);
+            return NULL;
+        }
+    
+    }
+    return data;
+}
