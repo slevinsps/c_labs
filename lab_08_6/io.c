e39a053a7e74993011af6df3ede3de7f8c890c5c
Ivan <spasenovivan@mail.ru>
2017-10-12 18:50:57 +0300

    add new folder

---
+++ lab_08_6/io.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "defines.h"
+#include "create_free_matrix.h"
+        
+int read_matrix(FILE *f,double ***matrix1,int *n1, int *m1)
+{	
+    int err = OK;
+    if (fscanf(f,"%d %d",n1,m1)==2 && *n1>0 && *m1>0)
+    {
+        *matrix1 = allocate_matrix_row(*n1,*m1);
+        for (int i = 0; i<*n1;i++)
+        {
+            for (int j = 0; j<*m1;j++)
+            {
+                if (fscanf(f,"%lf",&(*matrix1)[i][j]) != 1)
+                {
+                    err = INVALID_NUMBER;
+                    break;
+                }
+            }
+            if (err == INVALID_NUMBER)
+            {
+                free_matrix_rows(*matrix1,*n1);
+                break;
+            }
+        }            
+    }    
+    else
+    {
+        err = INVALID_ARGUMENT_MATRIX;
+    }
+    return err;
+}
+
+void print_matrix(FILE *f,double **matrix1,int n1, int m1)
+{
+    for (int i = 0; i<n1;i++)
+    {
+        for (int j = 0; j<m1;j++)
+        {
+            fprintf(f,"%f ",matrix1[i][j]);
+        }
+        fprintf(f,"\n");
+    }
+}
---
+++ lab_08_6/multiply_matrix.c
@@ -0,0 +1,31 @@
+#include "defines.h"
+#include "create_free_matrix.h"
+
+int multiply_matrix(double **matrix1, double **matrix2, int n1, int m1,int n2, int m2,double ***matrix3)
+{
+    int err = OK;
+    if (n1 == m2 && m1 == n2)
+    {
+        *matrix3 = allocate_matrix_row(n1,m2);
+        for (int i = 0; i<n1;i++)
+        {
+            for (int j = 0; j<m2;j++)
+            {
+                (*matrix3)[i][j] = 0;
+            }
+        }
+        for (int ii = 0; ii<n1; ii++)
+        {
+                for (int i = 0; i<n1;i++)
+                {
+                    for (int j = 0; j<n2;j++)
+                    {
+                        (*matrix3)[ii][i] += matrix1[ii][j] * matrix2[j][i];
+                    }
+                }        
+        }    
+    }    
+    else
+        err = DONT_RIGHT_SIZE;
+    return err;
+}
