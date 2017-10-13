22fd6d8f188295e43da61c24c4f4170916c15b27
Spasenov Ivan <spasenovivan@mail.ru>
2017-10-13 15:00:49 +0300

    Update io.c

--- lab_08_06/io.c
+++ lab_08_06/io.c
@@ -3,38 +3,26 @@
 #include "create_free_matrix.h"
         
 int read_matrix(FILE *f,double ***matrix1,int *n1, int *m1)
-{	
-    int err = OK;
-    if (fscanf(f,"%d %d",n1,m1)==2 && *n1>=0 && *m1>=0)
+{
+    if (fscanf(f, "%d %d", n1, m1) != 2)
     {
-		*matrix1 = allocate_matrix_row(*n1,*m1);
-		if (matrix1)
-		{
-			for (int i = 0; i<*n1;i++)
-			{
-				for (int j = 0; j<*m1;j++)
-				{
-					if (feof(f) == 0 && fscanf(f,"%lf",&(*matrix1)[i][j]) != 1)
-					{
-						err = INVALID_NUMBER;
-						break;
-					}
-				}
-				if (err == INVALID_NUMBER)
-				{
-					free_matrix_rows(*matrix1,*n1);
-					break;
-				}
-			}  
-		}
-		else
-			err = MEMORY_ERROR;
-    }    
-    else
-    {
-        err = INVALID_ARGUMENT_MATRIX;
+        return -1;
     }
-    return err;
+    if ((*n1 <= 0) || (*m1 <= 0))
+    {
+        return -2;
+    }
+    for (int i = 0; i < n1; i++)
+    {
+        for (int j = 0; j < m1; j++)
+        {
+            if (fscanf(f, "%lf", (matrix1[i] + j)) != 1)
+            {
+                return -3;
+            }
+        }
+    }
+    return 0;
 }
 
 void print_matrix(FILE *f,double **matrix1,int n1, int m1)
