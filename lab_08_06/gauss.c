2fb372e8787d6fc56cc1f39e9f78c2696072c0a5
Ivan <spasenovivan@mail.ru>
2017-10-15 15:54:06 +0300

    tets

--- lab_08_06/gauss.c
+++ lab_08_06/gauss.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <assert.h>
     
 #include "create_free_matrix.h"
 #include "io.h"
@@ -24,6 +25,7 @@ void creat_edin(double **edin, int n)
 // также делит элемент столбца единичной матрицы
 void gauss_divide(double *row, int number, double *chislo, int n1)
 {
+	assert(number >= 0);
     double div = row[number];
     *chislo = *chislo / div; // изменение значения элемента единичной матрицы
     
@@ -37,6 +39,7 @@ void gauss_divide(double *row, int number, double *chislo, int n1)
 // также вычитает один элемент столбца единичной матрицы из другого
 void subtraction(double *row1, double *row2, int number, double *chislo1, double *chislo2, int n1)
 {
+	assert(number >= 0);
     double sub = row1[number];
     
     for (int i = 0;i < n1;i++)
@@ -50,32 +53,32 @@ void subtraction(double *row1, double *row2, int number, double *chislo1, double
 // также меняет местами элемент столбца единичной матрицы
 // также здесь происходит проверка на нулевой определитель, смысл в том,
 // что если внизу нет больше таких строк, с ненулевым элементом, то значит, что определитель равен нулю
-int choos_not_zero_element(double ***matrix1, int column, int n, double **edin, int column_edin, double *arr_operations)
+int choos_not_zero_element(double **matrix1, int column, int n, double **edin, int column_edin, double *arr_operations)
 {    
-    double eps = 0.000000000001;
+    double eps = 0.000000001;
     int err = OK;
     double k;
-    int bool1 = 0;
     double *row;
+	int max_i = column;
     for (int i = column; i < n; i++)
     {
-        if (fabs((*matrix1)[i][column]) > eps)
-        {
-            bool1 = 1;
-            row = (*matrix1)[i];
-            (*matrix1)[i] = (*matrix1)[column];
-            (*matrix1)[column] = row;
-            k = edin[i][column_edin];
-            edin[i][column_edin] = edin[column][column_edin]; 
-            edin[column][column_edin] = k;
-            *arr_operations = i; // запись в массив операций
-            break;
-        }
+        if (matrix1[i][column] > matrix1[max_i][column])
+			max_i = i;
     }
-    if (!bool1)
+	if (fabs(matrix1[max_i][column]) > eps)
     {
-        err = DETERMINATE_0;
-    }       
+        row = matrix1[max_i];
+        matrix1[max_i] = matrix1[column];
+        matrix1[column] = row;
+	
+        k = edin[max_i][column_edin];
+        edin[max_i][column_edin] = edin[column][column_edin]; 
+        edin[column][column_edin] = k;
+	
+        *arr_operations = max_i; // запись в массив операций
+    }
+	else
+		err = DETERMINATE_0;     
     return err;
 }
 
@@ -98,7 +101,7 @@ int gauss(double **matrix1, double ***edin, int n1, int m1)
             for (int j = 0;j < n1;j++)
             {            
                 k++;
-                err = choos_not_zero_element(&matrix1, j, n1, *edin, 0, &arr_operations[k]);
+                err = choos_not_zero_element(matrix1, j, n1, *edin, 0, &arr_operations[k]);
                 //print_matrix(stdout,matrix1,n1,n1);
                 //printf("выше выбор\n");
                 if (err == DETERMINATE_0)
