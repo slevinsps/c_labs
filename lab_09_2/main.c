88b7b90ec1e606def1e3dc19b31b534f7a3fe2fe
U-VLAB\user-lab01 <user-lab01@LAB-01-47.vlab.bmstu.ru>
2017-11-15 14:52:57 +0300

    test

--- lab_09_2/main.c
+++ lab_09_2/main.c
@@ -7,7 +7,7 @@
 
 
 
-/* 
+
 int main(int argc, char **argv)
 {
     FILE *f1;
@@ -56,10 +56,8 @@ int main(int argc, char **argv)
     return err;
 }
 
- */
 
-
-int main(int argc, char **argv)
+/* int main(int argc, char **argv)
 {
     char *line = NULL;
     char *res = NULL;
@@ -118,4 +116,4 @@ int main(int argc, char **argv)
     }
     //ferror?
     return rc;
-} 
+}  */
