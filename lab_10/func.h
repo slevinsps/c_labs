694654926c7797ec126a0c2257ead2ff3e532082
U-VLAB\user-lab01 <user-lab01@LAB-01-69.vlab.bmstu.ru>
2017-11-24 11:30:54 +0300

    test

--- lab_10/func.h
+++ lab_10/func.h
@@ -6,12 +6,12 @@ struct node
 	void *data;
 	node_t *next;
 };
-void* pop_front(node_t **head);
+void *pop_front(node_t **head);
 void insert(node_t **head, node_t *elem, node_t *before);
-node_t* reverse(node_t *head);
-void front_back_split(node_t* head, node_t** back);
-node_t* sorted_merge(node_t **head_a, node_t **head_b, int (*comparator)(const void *, const void *));
+node_t *reverse(node_t *head);
+void front_back_split(node_t *head, node_t **back);
+node_t *sorted_merge(node_t **head_a, node_t **head_b, int (*comparator)(const void *, const void *));
 
-node_t* sort(node_t *head_a, int (*comparator)(const void *, const void *));
+node_t *sort(node_t *head_a, int (*comparator)(const void *, const void *));
 
 #endif // FUNC
