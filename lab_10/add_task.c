3279edb364438235d2d13f4231c4a419887bc662
Ivan <spasenovivan@mail.ru>
2017-11-26 00:43:47 +0300

    add

---
+++ lab_10/add_task.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "func.h"
+
+#define SIZE_BUF 200
+# define SEP "\"., ?!\n" 
+
+const char *strchr1(const char *str, char symbol)
+{
+    const char *c = NULL;
+    int len = strlen(str);
+    for (int i = 0; i < len; i++)
+    {
+        if (str[i] == symbol)
+        {
+            c = &(str[i]);
+            break;
+        }
+    }
+    return c;
+}
+
+int count_words(const char *str)
+{
+    int n = 0;
+    int len = strlen(str);
+    for (int i = 0; i < len; i++)
+    {
+        if (!strchr1(SEP, str[i]) && (i == 0 || strchr1(SEP, str[i - 1])))
+            n++;
+    }
+    return n;
+}
+
+void split_words(char *str, char **words, int ind)
+{
+    int len = strlen(str);
+    for (int i = 0; i < len; i++)
+    {
+        if (!strchr1(SEP, str[i])) 
+        {
+            if (i == 0 || str[i - 1] == 0)
+            {
+                words[ind] = &(str[i]);
+                ind++;
+            }
+        }
+        else
+            str[i] = 0;
+    }
+}
+
+void load_list_int(node_t *head, char **words, int n)
+{
+    head->data = words[0];
+    head->next = NULL;
+    node_t *tmp;
+    for (int i = 1; i < n; i++)
+    {
+        tmp = malloc(sizeof(node_t));
+        tmp->next = NULL;
+        tmp->data = words[i];
+        
+        head->next = tmp;
+        head = head->next;
+    }    
+}
+
+void read_text(FILE *f, char ***words, int *n)
+{
+	int ind;
+	char *string = malloc(SIZE_BUF);
+	int size_arr = 0;
+	while(fgets(string, 400, f))
+    {
+        ind = *n;
+        *n += count_words(string);
+		if (*n > size_arr)
+		{
+			size_arr = *n * 2;
+			*words = realloc(*words, size_arr * sizeof(char *));	
+		}
+		split_words(string, *words, ind);
+        string = malloc(SIZE_BUF);
+    }
+}
+
+int comp1(const void *a, const void *b)
+{
+  return strcmp((char *)a, (char *)b);
+} 
+
+void print_list(FILE *f, node_t *head)
+{
+    while (head)
+    {
+        fprintf(f, "%s\n", (char *)(head->data));
+        head = head->next;
+    }
+}
--- lab_10/main.c
+++ lab_10/main.c
@@ -1,9 +1,73 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include "func.h"
+#include "add_task.h"
 
-int main(void)
+#define SIZE_BUF 200
+# define SEP "\"., ?!\n" 
+# define NOT_ALL_ARGUMENTS -1
+# define NO_FILE -2
+# define OK 0
+
+
+/* Условие задачи */
+// Из файла f1 считывается текст, в котором вычленяются
+// слова, затем они сортируются по алфавиту и записываются
+// в файл f2
+int main(int argc, char **argv)
 {
-	return 0;
+	FILE *f1;
+	FILE *f2;
+	
+	int err = OK;
+	int n = 0;
+    char **words = NULL;	
+	if (argc != 3)
+    {
+        printf("./app.exe input.txt output.txt \n");
+        err = NOT_ALL_ARGUMENTS;
+    }
+	else 
+    {
+        f1 = fopen(argv[1], "r");
+        if (f1 == NULL)
+        {  
+            printf("NO FILE 1\n");
+            err = NO_FILE;
+        }
+        else
+        {
+            f2 = fopen(argv[2], "w");
+            if (f2 == NULL)
+            {
+                printf("NO FILE 2\n");
+                err = NO_FILE;
+            }
+            else
+            {
+				read_text(f1, &words, &n);
+				node_t *head = malloc(sizeof(node_t));
+				node_t *res_sort;
+				node_t *copy;
+				load_list_int(head, words, n);
+				res_sort = sort(head, comp1);
+				printf("Done!\n");			
+				print_list(f2, res_sort);
+				while (res_sort)
+				{
+					copy = res_sort;
+					res_sort = res_sort->next;
+					free(copy);
+				}
+				free(words);
+				fclose(f2);
+			}
+			fclose(f1);
+		}
+	}
+	return err;
 }
+
+
 
