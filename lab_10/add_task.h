07b769eb414d8d527f5a398ed37e8fb2a63cc148
Spasenov Ivan <spasenovivan@mail.ru>
2017-11-27 12:59:16 +0300

    test

--- lab_10/add_task.h
+++ lab_10/add_task.h
@@ -5,7 +5,7 @@ const char *strchr1(const char *str, char symbol);
 int count_words(const char *str);
 void split_words(char *str, char **words, int ind);
 void load_list_int(node_t *head, char **words, int n);
-int comp1(const void * a, const void * b);
+int comp1(const void *a, const void *b);
 void print_list(FILE *f, node_t *head);
 void read_text(FILE *f, char ***words, int *n);
 
