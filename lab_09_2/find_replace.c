b32da9bd1f0d0483f8344327d73e5247d38cfaaa
U-VLAB\user-lab01 <user-lab01@LAB-01-58.vlab.bmstu.ru>
2017-11-29 16:43:38 +0430

    test

--- lab_09_2/find_replace.c
+++ lab_09_2/find_replace.c
@@ -107,6 +107,72 @@ char* str_replace1(const char *source, const char *search, const char *replace)
 }
 
 
+
+
+char* str_replace(const char *source, const char *search, const char *replace)
+{
+    if ((source == NULL) || (search == NULL) || (replace == NULL))
+        return NULL;
+    
+    char *s = NULL;
+    int len1;
+    
+    int len2 = strlen1(search);
+    int len3 = strlen1(replace);
+    int len_res = strlen1(source);
+    
+    int pos1 = 0;
+    int pos2 = 0;
+    int j;
+    
+    char *new_source = malloc(len_res+1);
+    
+    
+    for (int i = 0; i < len_res; i++)
+    {
+        new_source[i] = source[i];        
+    }
+    new_source[len_res] = 0;
+    
+    while (find_underline(new_source, search, &pos1, &pos2))
+    {
+        len1 = strlen1(new_source);
+        len_res = len1 - len2 + len3;
+        s = malloc(len_res+1);
+        
+        for (int i = 0; i < pos1; i++ )
+        {
+            s[i] = new_source[i];
+        }
+        
+        for (int i = pos1; i < pos1 + len3; i++ )
+        {
+            s[i] = replace[i-pos1];
+        }
+        
+        j = pos2;
+        for (int i = pos1 + len3; i < len_res; i++ )
+        {
+            
+            s[i] = (new_source)[j];
+            j++;
+        }    
+        s[len_res] = 0;
+        new_source = realloc(new_source,len_res+1);
+        for (int i = 0; i < len_res; i++)
+        {
+            new_source[i] = s[i];
+        }
+        free(s);
+        new_source[len_res] = 0;
+    } 
+    return new_source;
+}
+
+
+
+
+
 void process(char *res, const char *replace, const char *source, const char *search, int *i, int *k, int m, int l)
 /*Основная функция поиска и замены подстроки в строке*/
 {
@@ -130,7 +196,7 @@ void process(char *res, const char *replace, const char *source, const char *sea
     res[*k] = 0;
 }
 
-char *str_replace(const char *source, const char *search, const char *replace)
+char *str_replace2(const char *source, const char *search, const char *replace)
 {
     char *res;
     int n, m, l;
