f3aaf198e9e07705e406235f23cccb531a69077b
Ivan <spasenovivan@mail.ru>
2017-12-21 23:53:08 +0300

    test

--- lab_11/my_snprintf.h
+++ lab_11/my_snprintf.h
@@ -2,5 +2,5 @@
 # define FUNC
 int read_specificators(const char *format, int *counter);
 void to_upp(char *s);
-int my_sprintf(char *string, size_t n, const char *format, ...);
+int my_snprintf(char *string, size_t n, const char *format, ...);
 # endif // FUNC
